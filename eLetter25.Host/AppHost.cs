7ad3f09d6e2d7d6254f2512d9583f712ef81ff4b
Elias <elias.kist@web.de>
2026-01-06 21:36:29 +0100

    Integrate Keycloak authentication and update project references; add settings for app host

--- eLetter25.Host/AppHost.cs
+++ eLetter25.Host/AppHost.cs
@@ -2,12 +2,20 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var keycloak = builder.AddKeycloak("keycloak", 8080)
+    .WithDataVolume()
+    .WithOtlpExporter();
+
 var sql = builder.AddSqlServer("sqlserver")
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent);
 
 var db = sql.AddDatabase("eletter25db");
 
-builder.AddProject<eLetter25_API>("letterAPI").WithReference(db);
+builder.AddProject<eLetter25_API>("letterAPI")
+    .WithReference(keycloak)
+    .WithReference(db)
+    .WaitFor(keycloak)
+    .WaitFor(db);
 
 builder.Build().Run();
